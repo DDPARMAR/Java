c65ee52a6b0e65787cbe71a6931dbc934eaa2b2c
user404e <131599479+user404e@users.noreply.github.com>
2023-05-26 13:13:33 +0000

    Update oddseries.java

--- oddseries.java
+++ oddseries.java
@@ -1,10 +1,11 @@
 public class oddseries {
     public static void main(String[] args) {
-        int num = 34;
-        while(num<=432){
-            if(num%2==0){
-                System.out.println(num);
-                num = num + 2 ;
+        for(int i = 32;i<=432;i++){
+            if(i%2!=0){
+                continue;
+            }
+            else{
+                System.out.println(i);
             }
         }
     }
