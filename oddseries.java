50b4fc5db72a534e75beba38298d08efd12300d2
DDPARMAR <105881760+DDPARMAR@users.noreply.github.com>
2023-05-26 18:30:29 +0530

    Add files via upload

---
+++ oddseries.java
@@ -0,0 +1,11 @@
+public class oddseries {
+    public static void main(String[] args) {
+        int num = 34;
+        while(num<=432){
+            if(num%2==0){
+                System.out.println(num);
+                num = num + 2 ;
+            }
+        }
+    }
+}
