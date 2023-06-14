be0e8b31d927bbab4bc0995b066491c628f0f079
user404e <131599479+user404e@users.noreply.github.com>
2023-06-14 13:13:38 +0000

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -3,7 +3,6 @@ import javax.swing.JFrame;
 public class practice{
     public static void main(String[] args) {
        JFrame j = new JFrame("Dev");
-       j.setVisible(true);
-       j.setSize(50000,50000);
+      
   }
 }
