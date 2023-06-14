897c162121ba4f5949b602371d053e46233687f7
DDPARMAR <105881760+DDPARMAR@users.noreply.github.com>
2023-06-14 18:41:21 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class practice{
+    public static void main(String[] args) {
+       JFrame j = new JFrame("Dev");
+       j.setVisible(true);
+       j.setSize(50000,50000);
+  }
+}
