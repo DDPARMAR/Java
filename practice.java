cf0fa03fb3952516f9e40e7386d81f157768d9ca
user404e <131599479+user404e@users.noreply.github.com>
2023-06-19 13:29:02 +0000

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,10 +1,15 @@
 import java.io.FileInputStream;
+import java.io.IOException;
 public class practice{
     public static void main(String[] args) {
         FileInputStream obj = new FileInputStream("txt.txt");
-        int a;
-        while((a=obj.read())!= -1){
-            System.out.println((char)a);
+        try {
+            int a;
+            while((a=obj.read())!= -1){
+                System.out.println((char)a);
+            }
+        } catch (IOException e) {
+            
         }
     }
 }
